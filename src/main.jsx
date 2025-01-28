64ad6311056ddd88521d5174c8d6511e8ab126da
ChukwuebukaNdeche <emmanuelndeche@gmail.com>
2025-01-28 08:28:53 +0100

    Fix an issue

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import {BrowserRouter} from "react-router-dom"
+import {HashRouter} from "react-router-dom"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-const isProduction = process.env.NODE_ENV === "production";
-const basename = isProduction ? "/portfolio-app" : "/";
+/*const isProduction = process.env.NODE_ENV === "production";
+const basename = isProduction ? "/portfolio-app" : "/";*/
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename={basename}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 )
